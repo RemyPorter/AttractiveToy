9152f11421ca30608f5bf2d36276c37090449629
remy <t3knomanser@gmail.com>
2021-06-27 14:11:02 -0400

    Multiple Groups, Improved feedback
    Visual feedback is a bit better. Multiple affininty groups are supported, with a configurable probability fro how frequently you jump groups.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Inspired by [this video](https://www.youtube.com/watch?v=kbKtFN71Lfs), I whipped
 
 To use the toy, go [here](https://remyporter.github.io/AttractiveToy/), then: 
 
+
+# Basic Editing
 * `click+drag`: You can move the attractor points by moving your mouse close to them, and clicking and dragging when they appear red.
 * `click`: You can add new attractor points by clicking anywhere else on the canvas.
 * ` `: You can randomly change the color of the last attractor you added or grabbed with the spacebar.
@@ -10,6 +12,15 @@ To use the toy, go [here](https://remyporter.github.io/AttractiveToy/), then:
 * `-`: you can decrease the "lerp" step with "-"
 * `p`: you can hide the "pings" for the attractors with this key, useful if you want to save your image.
 * `c`: you can clear/reset the drawing with "c".
+
+# Group Management
+This tool supports multiple affinity groups.
+
+* [`1`, `2`, …, `6`]: if the affinity group is not currently on the render, adds it. If the affinity group is on the render, but is not the active editing group, switches to it. If the affinity group *is* the active editing group, removes it from the render.
+* `UP`/`DOWN` arrows: change the probability that we switch between affinity groups. 
+
+The active editing group also changes when you click on one of its control points.
+
 
 **Saving**: right click on the canvas and let your browser do the work.
 
--- sketch.js
+++ sketch.js
@@ -1,29 +1,146 @@
 const N = 3;
-const SPEED = 4000;
-const GRAB_RADIUS=0.05;
+const SPEED = 6000;
+const GRAB_RADIUS=0.04;
+const COLOR_MAP = {
+  "1": "#ff0000",
+  "2": "#00FF00",
+  "3": "#0000FF",
+  "4": "#FFFF00",
+  "5": "#FF00FF",
+  "6": "#00FFFF"
+}
 let grabbed = null;
 let grabbable = null;
 let lastGrabbed = null;
 let showPings = true;
 let lerpDist = 0.5;
 
-let attractors = [
-];
+function choice(arr) {
+  return arr[Math.floor(Math.random()*arr.length)]
+}
+
+class AttractorGroup {
+  constructor() {
+    this.attractors = [
+      Vector.Random(5), Vector.Random(5), Vector.Random(5)
+    ];
+    this.nextPoint = () => choice(this.attractors);
+  }
+
+  removeAttractor(a) {
+    this.attractors = this.attractors.filter((att) => att != a);
+  }
+}
+
+class GroupManager {
+  constructor() {
+    this.groups = {"1": new AttractorGroup()};
+    this.editGroup = "1";
+    this.activeGroup = "1";
+    this.groupSwitchProbability = 0.25;
+  }
+
+  toggleGroup(id) {
+    if (this.groups[id] && id == this.editGroup && Object.keys(this.groups).length > 1) {
+      delete this.groups[id];
+      this.editGroup = Object.keys(this.groups)[0];
+    } else if (this.groups[id]) {
+      this.editGroup = id;
+    } else {
+      this.groups[id] = new AttractorGroup();
+      this.editGroup = id;
+    }
+  }
+
+  groupNames() { return Object.keys(this.groups); }
+  getGroup(id) { return this.groups[id]; }
+
+  allAttractors() {
+    let res = [];
+    Object.keys(this.groups).forEach((k) => {
+      if (this.groups[k]) {
+        res = res.concat(this.groups[k].attractors);
+      }
+    });
+    return res;
+  }
+
+  addAttractor(att) {
+    this.groups[this.editGroup].attractors.push(att);
+  }
+
+  removeAttractor(att) {
+    this.groups[this.editGroup].removeAttractor(att);
+    this.switch();
+  }
+
+  onGrab(att) {
+    Object.keys(this.groups).forEach((k) => {
+      if (this.groups[k].attractors.indexOf(att) > -1) {
+        this.editGroup = k;
+      }
+    })
+  }
+
+  switch() {
+    let k = Object.keys(this.groups);
+    this.activeGroup = choice(k);
+  }
+
+  nextPoint() {
+    if (random() <= this.groupSwitchProbability) {
+      this.switch();
+    }
+    if (!this.groups[this.activeGroup]) {
+      this.switch();
+    }
+    return this.groups[this.activeGroup].nextPoint();
+  }
+}
+
+function drawGroup(gm, id) {
+  let g = gm.getGroup(id);
+  if (g) {
+    let attractors = g.attractors;
+    if (showPings) {
+      attractors.forEach((a) => {
+        strokeWeight(1.0/width);
+        stroke(COLOR_MAP[id]);
+        if (a == grabbable) {
+          stroke(255, 255, 255);
+        }
+        vfill(a);
+        if (a == lastGrabbed) {
+          strokeWeight(3.0/width);
+          fill(255);
+        }
+        ellipse(a.e(1), a.e(2), 5.0/width, 5.0/height);
+      });
+      noFill();
+    }
+  }
+}
+
+function drawAttractors(gm) {
+  let gs = gm.groupNames();
+  if (showPings) {
+    gs.forEach((id) => drawGroup(gm, id));
+  }
+}
+
 
 let tracepoint = Vector.Random(5);
+let gm = new GroupManager();
 
 function step() {
-  let dest = attractors[Math.floor(Math.random()*attractors.length)];
+  let dest = gm.nextPoint();
   let inv = dest.subtract(tracepoint);
   inv = inv.multiply(lerpDist);
   tracepoint = tracepoint.add(inv);
 }
 
 function seedAttractors() {
-  attractors = [];
-  for (let i = 0; i < N; i++) {
-    attractors.push(Vector.Random(5));
-  }
+  gm = new GroupManager();
   tracepoint = Vector.Random(5);
 }
 
@@ -44,18 +161,7 @@ function vfill(vec) {
 function draw() {
   scale(width, height);
   console.log(grabbable);
-  if (showPings) {
-    attractors.forEach((a) => {
-      strokeWeight(1.0/width);
-      if (a == grabbable) {
-        stroke(255, 0, 0);
-      } else {
-        stroke(255);
-      }
-      vfill(a);
-      ellipse(a.e(1), a.e(2), 5.0/width, 5.0/height);
-    });
-  }
+  drawAttractors(gm);
   noStroke();
   for (let i = 0; i < SPEED; i++) {
     vfill(tracepoint);
@@ -72,8 +178,7 @@ function keyPressed() {
     showPings = !showPings;
     background(0);
   } else if (key == "r") {
-    if (attractors.length > 3)
-      attractors = attractors.filter((a) => a != lastGrabbed);
+    gm.removeAttractor(lastGrabbed);
     background(0);
   } else if (key == "=" || key == "+") {
     lerpDist += 0.01;
@@ -86,6 +191,15 @@ function keyPressed() {
     lastGrabbed.elements[3] = random();
     lastGrabbed.elements[4] = random();
     background(0);
+  } else if (Object.keys(COLOR_MAP).indexOf(key) > -1) {
+    gm.toggleGroup(key);
+    background(0);
+  } else if (keyCode == UP_ARROW) {
+    gm.groupSwitchProbability += 0.01;
+    background(0);
+  } else if (keyCode == DOWN_ARROW) {
+    gm.groupSwitchProbability -= 0.01;
+    background(0);
   }
 }
 
@@ -94,7 +208,7 @@ function mouseClicked() {
   let a = Vector.Random(5);
   a.elements[0] = mouseX/width;
   a.elements[1] = mouseY/height;
-  attractors.push(a);
+  gm.addAttractor(a);
   background(0);
   tracepoint = Vector.Random(5);
   lastGrabbed = a;
@@ -102,6 +216,7 @@ function mouseClicked() {
 
 function nearMouse() {
   let v = $V([mouseX/width, mouseY/height]);
+  let attractors = gm.allAttractors();
   for (let i = 0; i < attractors.length; i++) {
     let av = attractors[i];
     let dv = $V([av.e(1), av.e(2)]);
@@ -117,19 +232,17 @@ function mousePressed() {
   let nm = nearMouse();
   if (nm != null) {
     grabbed = nm;
-    console.log("grabbed");
+    gm.onGrab(nm);
   }
 }
 
 function mouseReleased() {
-  console.log("Released");
   lastGrabbed = grabbed;
   grabbed = null;
 }
 
 function mouseDragged() {
   if (grabbed != null) {
-    console.log("moving");
     background(0);
     grabbed.elements[0] = mouseX/width;
     grabbed.elements[1] = mouseY/height;
